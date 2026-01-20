7a692680e6d85ac3bde5252c8ec48f6281157065
Top <Top@WIN-EGPVL3TPSSD>
2026-01-20 15:43:07 +0500

    vd

--- 123124/Source.cpp
+++ 123124/Source.cpp
@@ -4,38 +4,40 @@ class Integer
 {
 public:
 	Integer();
-	Integer(unsigned units, bool sign);
-	Integer(int num);
+	Integer(unsigned units);
+	//Integer(int num);
 
-	void SetSign(bool sign);
-	bool GetSign()const;
+	//void SetSign(bool sign);
+	//bool GetSign()const;
 
-	void Units(unsigned units);
-	unsigned Units()const;
+	//void Units(unsigned units);
+	//unsigned Units()const;
 
-	auto ToString()const -> std::string;
-	auto ToInt()const -> int;
+	unsigned GetUnits()const;
 
-	bool IsEven()const;
-	bool IsOdd()const;
-	bool IsPrime()const;
-	bool IsNegative()const;
-	bool IsPositive()const;
+	//auto ToString()const -> std::string;
+	//auto ToInt()const -> int;
+
+	//bool IsEven()const;
+	//bool IsOdd()const;
+	//bool IsPrime()const;
+	//bool IsNegative()const;
+	//bool IsPositive()const;
 
 	Integer Summ(const Integer& other) const;
 	Integer Subtract(const Integer& other) const;
 	Integer Multiply(const Integer& other) const;
-	Integer Divide(const Integer& other) const;
-	Integer Power(const Integer& other) const;
+	//Integer Divide(const Integer& other) const;
+	//Integer Power(const Integer& other) const;
 
 	Integer operator+(const Integer& other)const
 	{
 		return /*this->*/Summ(other);
 	}
 
-	friend Integer operator-(const Integer& lso, const Integer& rso)
+	Integer operator-(const Integer& other)
 	{
-		return lso.Subtract(rso);
+		return Subtract(other);
 	}
 
 	Integer operator--(void)
@@ -70,15 +72,45 @@ private:
 
 Integer::Integer()
 {
+	units_ = 0;
+	sign_ = 0;
+}
+
+Integer::Integer(unsigned units)
+{
+	units_ = units;
+	sign_ = 0;
 }
 
 
 int main()
 {
-	Integer val1{ 15 };
-	Integer val2{ 11 };
+	Integer val1{ 10 };
+	Integer val2{ 9 };
 	Integer res;
-	res = 7 - val1;
+	res = val1 + val2;
+	std::cout << res.GetUnits() << "\n";
+	res = val1 - val2;
+	std::cout << res.GetUnits() << "\n";
 }
 
+Integer Integer::Summ(const Integer& other) const
+{
+	return Integer(this->units_ + other.units_);
+}
 
+Integer Integer::Subtract(const Integer& other) const
+{
+	return Integer(this->units_ - other.units_);
+}
+
+Integer Integer::Multiply(const Integer& other) const
+{
+
+	return Integer();
+}
+
+unsigned Integer::GetUnits() const
+{
+	return units_;
+}
